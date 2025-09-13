19fc46c7b8d5ed761aa8bbe10e7467fcf3d4ad65
temptationkentos57 <kreetlannoo@hotmail.com>
2025-09-13 17:05:41 +0200

    Fix greeting message to use English translation for clarity.

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Chào mừng đến với Ứng Dụng Tìm Việc Nửa Giờ!');
+  res.send('Welcome to the Half-Hour Job Finder Application!');
 });
 
 app.listen(port, () => {
-  console.log(`Server chạy trên http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
